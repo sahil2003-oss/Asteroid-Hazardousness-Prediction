2407f45c7f64c56e3450657c1553c2bd02a92bf4
Sahil Singh Chouhan <sahilsinghchouhan2003@gmail.com>
2026-09-22 23:47:19 +0530

    All the files are Added

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+import pandas as pd
+from pydantic import BaseModel, Field
+import joblib
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
+input_features = [
+    "Minimum_Orbit_Intersection",
+    "Absolute_Magnitude",
+    "Maximum_Possible_Estimated_Diameter",
+]
+
+model = joblib.load("asteroid_hazard_model.pkl")
+
+
+# Pydantic Model: Responsible for performing Input Data Validaiton
+class Features(BaseModel):
+    Minimum_Orbit_Intersection: float = Field(...)
+    Absolute_Magnitude: float = Field(..., ge=11.16, le=32.1)
+    Maximum_Possible_Estimated_Diameter: float = Field(..., ge=0, le=35)
+
+
+@app.get("/")
+def greet():
+    return "hello guys!"
+
+
+@app.post("/make_prediction")
+def predict(data: Features):
+    input_row = pd.DataFrame([dict(data)], columns=input_features)
+    prediction = model.predict(input_row)
+    probablity = model.predict_proba(input_row)
+    if int(prediction[0]) == 0:
+        hazardousness = "Non-Hazardous"
+    else:
+        hazardousness = "Hazardous"
+    return {
+        "IS ASTEROID HAZARDOUS": hazardousness,
+        "PROBABLITY": probablity.tolist(),
+    }
